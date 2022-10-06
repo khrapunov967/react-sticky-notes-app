0cda064749df521bb5b453f478a286fc7e239804
Andrew Khrapunov <andrew.khrapunov967@gmail.com>
2022-10-06 19:43:11 +0700

    Make search bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import SearchBar from "./UI/SearchBar/SearchBar";
 
 function App() {
   return (
-    <div className='wrapper'>123</div>
+    <div className='wrapper'>
+      <SearchBar placeholder={"Search note..."}/>
+    </div>
   );
 }
 
---
+++ src/UI/SearchBar/SearchBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./SearchBar.scss";
+
+const SearchBar = ({...props}) => {
+    return (
+        <div className="search-bar">
+            <div className="search-bar__icon">
+                <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M221.09 64a157.09 157.09 0 10157.09 157.09A157.1 157.1 0 00221.09 64z" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="32" d="M338.29 338.29L448 448"/></svg>
+            </div>
+
+            <input {...props} type="text" name="search-stroke" className="search-bar__search-stroke" />
+        </div>
+    );
+};
+
+export default SearchBar;
